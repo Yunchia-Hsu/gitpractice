029134804bcf1864c80e5811e2b1d2d5eb1889b8
Yun-Chia <127295359+Yunchia-Hsu@users.noreply.github.com>
2024-02-18 19:10:03 +0800

    Update push_command.c

--- push_swap/push_command.c
+++ push_swap/push_command.c
@@ -49,3 +49,47 @@ void	pb(t_stack_node **b, t_stack_node **a, bool checker)
 	if (!checker)
 		write (1, "pb\n", 3);
 }
+
+// int main()
+// {
+//     t_stack_node *a;
+//     t_stack_node *b = NULL; 
+//     a = malloc(sizeof(t_stack_node));
+//     a->next = malloc(sizeof(t_stack_node));
+//     a->next->next = malloc(sizeof(t_stack_node));
+//     a->next->next->next = malloc(sizeof(t_stack_node));
+//     b = malloc(sizeof(t_stack_node));
+//     b->next = malloc(sizeof(t_stack_node));
+//     b->next->next = malloc(sizeof(t_stack_node));
+//     b->next->next->next = malloc(sizeof(t_stack_node));
+
+//     a->value = -1;
+//     a->next->value = 0;
+//     a->next->next->value = 1;
+//     a->next->next->next->value = 2;
+//     a->prev = NULL;
+//     a->next->prev = a;
+//     a->next->next->prev = a->next;
+//     a->next->next->next->prev = a->next->next;
+
+//     b->value = 4;
+//     b->next->value = 5;
+//     b->next->next->value = 6;
+//     b->next->next->next->value = 7;
+//     b->prev = NULL;
+//     b->next->prev = b;
+//     b->next->next->prev = b->next;
+//     b->next->next->next->prev = b->next->next;
+
+
+    
+//     printf("a stack: %d, %d, %d,%d\n", a->value, a->next->value, 
+//     a->next->next->value, a->next->next->next->value);
+//     printf("b stack: %d, %d, %d,%d\n", b->value, b->next->value, 
+//     b->next->next->value, b->next->next->next->value);
+//     push(&b, &a);
+//     printf("push a: %d, %d, %d\n", a->value, a->next->value, 
+//     a->next->next->value);
+//     printf("push b: %d, %d, %d, %d, %d\n", b->value, b->next->value, 
+//     b->next->next->value, b->next->next->next->value,b->next->next->next->next->value);
+// }
